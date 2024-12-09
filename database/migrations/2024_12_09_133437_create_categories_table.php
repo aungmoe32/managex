eb77f3d8dddc2df096e438d19d46c9c399b440bf
aungmoe32 <aungmoemyintthu@gmail.com>
2024-12-09 20:41:47 +0630

    add category migration

--- database/migrations/2024_12_09_133437_create_categories_table.php
+++ database/migrations/2024_12_09_133437_create_categories_table.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('body');
-            $table->foreignId('user_id')->nullable()->constrained()->onDelete('set null'); // Optional: link to users table
-            $table->foreignId('category_id')->nullable()->constrained()->onDelete('set null');
+            $table->string('name')->unique(); // Category name
             $table->timestamps();
         });
     }
@@ -26,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('categories');
     }
 };
