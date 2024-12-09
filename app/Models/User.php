acff23de61cfa6af325fd023f5715b61808b6e5b
aungmoe32 <aungmoemyintthu@gmail.com>
2024-12-09 20:55:26 +0630

    update model relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use App\Constants\Role;
 use Illuminate\Contracts\Auth\CanResetPassword;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -50,6 +51,11 @@ class User extends Authenticatable implements MustVerifyEmail, CanResetPassword
     {
         return $this->hasOne(Profile::class);
     }
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
     protected static function booted(): void
     {
         static::created(function (User $user) {
