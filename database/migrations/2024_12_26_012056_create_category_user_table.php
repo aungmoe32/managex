231b101c57ea201c2ea2d4a904ef13a9f4320dc9
aungmoe32 <aungmoemyintthu@gmail.com>
2025-01-05 20:37:19 +0630

    add uniques to category_user table

--- database/migrations/2024_12_26_012056_create_category_user_table.php
+++ database/migrations/2024_12_26_012056_create_category_user_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->unique(['user_id', 'category_id']);
             $table->timestamps();
         });
     }
